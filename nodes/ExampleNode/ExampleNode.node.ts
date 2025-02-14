452ad165f8782d0bc1b16171f088446479031ef5
lephuongbac <rockviet@gmail.com>
2025-02-14 23:51:31 +0700

    add new node

--- nodes/ExampleNode/ExampleNode.node.ts
+++ nodes/ExampleNode/ExampleNode.node.ts
@@ -3,74 +3,54 @@ import {
 	INodeExecutionData,
 	INodeType,
 	INodeTypeDescription,
-	NodeOperationError,
 } from 'n8n-workflow';
+import { faker } from '@faker-js/faker';
+declare global {
+	var faker: any;
+}
+globalThis.faker = faker as any;
 
 export class ExampleNode implements INodeType {
 	description: INodeTypeDescription = {
-		displayName: 'Example Node',
-		name: 'exampleNode',
+		displayName: 'Faker Node',
+		name: 'fakerNode',
 		group: ['transform'],
 		version: 1,
-		description: 'Basic Example Node',
+		description: 'Generates mock data using faker.js',
 		defaults: {
-			name: 'Example Node',
+			name: 'Faker Node',
+			color: '#772244',
 		},
 		inputs: ['main'],
 		outputs: ['main'],
 		properties: [
-			// Node properties which the user gets displayed and
-			// can change on the node.
 			{
-				displayName: 'My String',
-				name: 'myString',
-				type: 'string',
+				displayName: 'JSON Input',
+				name: 'jsonInput',
+				type: 'json',
 				default: '',
-				placeholder: 'Placeholder value',
-				description: 'The description text',
+				placeholder: '[{"name": "email", "rule":"faker.internet.email"}]',
+				description: 'The JSON input to define the mock data rules.',
 			},
 		],
 	};
 
-	// The function below is responsible for actually doing whatever this node
-	// is supposed to do. In this case, we're just appending the `myString` property
-	// with whatever the user has entered.
-	// You can make async calls and use `await`.
 	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
 		const items = this.getInputData();
+		const returnData: INodeExecutionData[] = [];
 
-		let item: INodeExecutionData;
-		let myString: string;
-
-		// Iterates over all input items and add the key "myString" with the
-		// value the parameter "myString" resolves to.
-		// (This could be a different value for each item in case it contains an expression)
-		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
-			try {
-				myString = this.getNodeParameter('myString', itemIndex, '') as string;
-				item = items[itemIndex];
-
-				item.json['myString'] = myString;
-			} catch (error) {
-				// This node should never fail but we want to showcase how
-				// to handle errors.
-				if (this.continueOnFail()) {
-					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
-				} else {
-					// Adding `itemIndex` allows other workflows to handle this error
-					if (error.context) {
-						// If the error thrown already contains the context property,
-						// only append the itemIndex
-						error.context.itemIndex = itemIndex;
-						throw error;
-					}
-					throw new NodeOperationError(this.getNode(), error, {
-						itemIndex,
-					});
-				}
+		for (let i = 0; i < items.length; i++) {
+			const jsonInput = this.getNodeParameter('jsonInput', i) as string;
+			const rules = JSON.parse(jsonInput);
+			const generatedData: { [key: string]: any } = {};
+			for (const rule of rules) {
+				// const fakerFunction = rule.rule.split('.').reduce((o: any, i: any) => o[i], faker);
+				generatedData[rule.name] = eval(rule.rule);
 			}
+
+			returnData.push({ json: generatedData });
 		}
 
-		return this.prepareOutputData(items);
+		return [returnData];
 	}
 }
